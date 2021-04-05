09403fd99cb35f78e07bbee9013bba5b14de4933
Peter Staev <peter@tangrasoft.com>
2021-04-05 14:54:27 +0300

    support for NS 8

--- index.ts
+++ index.ts
@@ -1,17 +1,17 @@
-import { Visibility } from "ui/enums";
+import { CoreTypes } from "@nativescript/core";
 
 import * as moment from "moment";
 
 export function booleanVisibilityConverter(value: boolean): string {
-    return value ? Visibility.visible : Visibility.collapse;
+    return value ? CoreTypes.Visibility.visible : CoreTypes.Visibility.collapse;
 }
 
 export function dataVisibilityConverter(value: string): string {
-    return value === "" || value === undefined || value === null ? Visibility.collapse : Visibility.visible;
+    return value === "" || value === undefined || value === null ? CoreTypes.Visibility.collapse : CoreTypes.Visibility.visible;
 }
 
 export function dataInverseVisibilityConverter(value: string): string {
-    return value === "" || value === undefined || value === null ? Visibility.visible : Visibility.collapse;
+    return value === "" || value === undefined || value === null ? CoreTypes.Visibility.visible : CoreTypes.Visibility.collapse;
 }
 
 export function formatDate(date: Date | string, format: string, emptyValue: string = " "): string { // NOTE: Do not use empty string as then labels get 0 height!
